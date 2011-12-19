3a89e11646fa53fcad566ece81bc88b4773c0e0e
akasoft <devnull@localhost>
2011-12-19 12:28:53 +0400

    Смена nmlc на 0.3.

--- config.bat
+++ config.bat
@@ -8,6 +8,6 @@ goto :EOF
 
 :: Setup
 :Setup
-set PATH=C:\MinGW\msys\1.0\bin;C:\MinGW\bin;C:\DOSUtil\Nml;%PATH%
+set PATH=C:\MinGW\msys\1.0\bin;C:\MinGW\bin;C:\DOSUtil\Nml3;%PATH%
 set NMLNAME=xussr
 set GRFFOLDER=%USERPROFILE%\Documents\OpenTTD\content_download\data\my\ecs
