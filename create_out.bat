db8c957ea2a11ba2e2eb64edf237a4606df9f639
akasoft <devnull@localhost>
2013-01-07 21:26:11 +0400

    Правильно формируем архивы. Игнорируем архив с репозиторием.

--- create_out.bat
+++ create_out.bat
@@ -12,9 +12,9 @@ call compile gethgrev
 7z a %NMLNAME%.%REPO_REVISION%.grf.7z %NMLNAME%.grf
 
 :: create src.7z arhive
-7z a -r %NMLNAME%.%REPO_REVISION%.src.7z docs\*.* lang\*.* src\*.*
+7z a -r %NMLNAME%.%REPO_REVISION%.src.7z docs\* lang\* src\*
 7z a %NMLNAME%.%REPO_REVISION%.src.7z ^
   *.bat %NMLNAME%.nfo %NMLNAME%.nml %CUSTOM_TAGS%
 
 :: create hg.7z arhive
-7z a -r %NMLNAME%.%REPO_REVISION%.hg.7z .hg\*.*
+7z a -r %NMLNAME%.%REPO_REVISION%.hg.7z .hg\*
