9eb063645c13a74e68fcd7b321cfbe23de6a8e70
George <georgevb@yandex.ru>
2022-12-26 19:04:12 +0300

    Fixes for the script for adjusting lists

--- scripts/Adjust2Master.pl
+++ scripts/Adjust2Master.pl
@@ -23,11 +23,13 @@ use open qw(:std :utf8);
 # If data is represented in the wrong section, then it should be commented there and moved to the right section
 # if data is represented several times, all the variants should be placed in the one place 
 
-my($header_template, $footer_template, $section_template, $key_template, $key_separator_master, $key_separator_slave) = ("\# begin main block.*?\n", "\# end main block", "\#\n\#.*?\n\#\n", "\[\/\]\{0\,2\}\\s\*\[a\-z\_0\-9\]\+\?\\s\*\:", "\:", "\:");
+my($header_template, $footer_template, $section_template, $key_template, $key_separator_master, $key_separator_slave) = 
+	("\# begin main block.*?\n", "\# end main block", "\#\n\#.*?\n\#\n", "\[\/\]\{0\,2\}\\s\*\[a\-z\_0\-9\.\]\+\?\\s\*", "\:", "\:");
 
-sub read_file($) {
-	my($fname) = (@_);
+sub read_file($$) {
+	my($fname, $separator) = (@_);
 	my($header, $footer, @sections) = ("", "");
+	my($count_sections, $count_keys) = (0, 0);
 	print("Parsing $fname ...");
 	open(FROMFILE, "<:encoding(UTF-8)", $fname) || die "Can't open: ".$fname;
 	my($str) = join("", <FROMFILE>);
@@ -36,13 +38,15 @@ sub read_file($) {
 	$header = $1 if($str =~ s/^(.*?$header_template)//igs);
 	$footer = $1 if($str =~ s/($footer_template.*?)$//igs);
 	while($str =~ s/^($section_template)(.*)$//igs) {
+		$count_sections++;
 		my($section, $key_list) = ($1, $2);
                 if($key_list =~ s/^(.*?)($section_template)(.*)$/$1/igs) {
 			$str = $2 . $3; 
 		} 	
 		my(%keys) = ();
 		$key_list .= "\n";
-		while($key_list =~ s/^($key_template)(.*?)\n//igs) {  
+		while($key_list =~ s/^($key_template$separator)(.*?)\n//igs) {
+			$count_keys++;  
 			my($key, $value) = ($1, $2);
 			if(exists $keys{$key}) {
 		        	@{$keys{$key}} = (@{$keys{$key}}, $value);
@@ -55,7 +59,7 @@ sub read_file($) {
 		%{$tmp} = %keys;
 		@sections = (@sections, $tmp);
 	}
-	print("success.\n");
+	print("success. Sections: $count_sections, keys: $count_keys\n");
 	return ($header, $footer, @sections);
 # returned structure:
 # Array of header, footer, sections. Each section is a hash of keys - values, where value is an array of values (because 
@@ -94,16 +98,16 @@ sub get_key_values($$$@) {
 
 sub parse_files($$) {
 	my($from_file, $to_file) = (@_);
-	my($header_master, $footer_master, @sections_master) = (read_file($from_file));
-	my($header_slave, $footer_slave, @sections_slave) = (read_file($to_file));
+	my($header_master, $footer_master, @sections_master) = (read_file($from_file, $key_separator_master));
+	my($header_slave, $footer_slave, @sections_slave) = (read_file($to_file, $key_separator_slave));
 	my($sec_number, $result, $section, %keys, $key, @values) = (1, "");
 	$result .= $header_slave;
 	foreach $section (@sections_master) {
 		%keys = %{$section};
 	        $result .= join("", @{$keys{"-Section-Header-"}});
-                print("Block #$sec_number\n");
+                print("$sec_number\.");
 		$sec_number++;
-		foreach $key (sort {uc($a) cmp uc($b)} keys(%keys)) {
+		foreach $key (sort {$a cmp $b} keys(%keys)) {
 			if($key ne "-Section-Header-") {
 				@values = get_key_values($key, $key_separator_master, $key_separator_slave, @sections_slave);
 				if(scalar(@values) == 2) {
@@ -117,16 +121,20 @@ sub parse_files($$) {
 				}
 			}
 # TODO check, if slave does not have unique values in the section. currently they are moved to the end of file 		
+# TODO place them just after the neares apropriate key, because it may be, for example, genders
 		}
 	}
 # look for slave-only values and add them
-	print("Last block - unused strings\n");
+	print("unused strings\n");
+	$sec_number = 1;
 	foreach $section (@sections_slave) {
+		print("$sec_number\.");
+		$sec_number++;
 		%keys = %{$section};
-		foreach $key (sort {uc($a) cmp uc($b)} keys(%keys)) {
+		foreach $key (sort {$a cmp $b} keys(%keys)) {
 			if($key ne "-Section-Header-") {
-				@values = get_key_values($key, $key_separator_master, $key_separator_slave, @sections_master);
-				if(scalar(@values) == 0) {
+				@values = get_key_values($key, $key_separator_slave, $key_separator_master, @sections_master);
+				if(scalar(@values) == 2) {
 					$result .= "\/\/ ERROR!!! Some values are not represented in the master file $from_file\n";
 					$result .= "\/\/ $key" . join("\n\/\/ $key", @{$keys{$key}}) . "\n";
 				} 
@@ -139,15 +147,14 @@ sub parse_files($$) {
 	open(TOFILE, ">:encoding(UTF-8)", "temp.tmp") || die("Can't create: temp.tmp");
 	print TOFILE $result;
 	close(TOFILE);
-	if (rename ($to_file, $to_file . ".bak")) {
-		print("$to_file renamed to $to_file\.bak\n");
+	if (unlink($to_file)) {
 		if (rename ("temp.tmp", $to_file)) { 
 			print("Result saved to $to_file\n");
 		} else { 
 			print("temp.tmp can\'t be renamed. Result saved to temp.tmp\n");
 		}
 	} else {
-		print("$to_file can\'t be renamed. result saved to temp.tmp. ");
+		print("$to_file can\'t be deleted. result saved to temp.tmp. ");
 	}
 }
 
@@ -155,7 +162,7 @@ sub main() {
 	print("Adjust file acording master file. v0.1\nParameters - master file, file to change, rules\n");
 	my($name_master, $name_slave, $rules) = ($ARGV[0], $ARGV[1], $ARGV[2]);
 	$name_master = "" if(not defined($name_master));
-	if(($name_master eq "") || (not(-e $name_master)) || (-d $name_master)) {
+	if(($name_master eq "") || (not(-e $name_master)) || (-d $name_master)) {                            
 		die("Can't find $name_master\n");
 	}
 	$name_slave = "" if(not defined($name_slave));
