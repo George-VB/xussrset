e9e35de0a5ea4815002de5983e2e0174d5339bc7
George <georgevb@yandex.ru>
2023-05-08 12:10:31 +0300

    #109 Fixed issues with cost and info

---
+++ compile-all.bat
@@ -0,0 +1,8 @@
+call  compile-addon.bat
+call  compile-diesel.bat
+call  compile-dmu.bat
+call  compile-electric.bat
+call  compile-emu.bat
+call  compile-rails.bat
+call  compile-steam.bat
+call  compile.bat
