603c65f5d590641289d12a66f2839b1c3e33d1ca
McModder <me@modder.pw>
2025-06-11 14:57:18 +0300

    improve compile.sh script

--- compile.sh
+++ compile.sh
@@ -7,10 +7,80 @@ NML_BASENAME=${NML_BASENAME:-xussr}
 NMLCOPTIONS=()
 MIN_COMPATIBLE_REVISION=${MIN_COMPATIBLE_REVISION:-1}
 CUSTOM_TAGS_FILE=${CUSTOM_TAGS_FILE:-custom_tags.txt}
-# get version and revision
-GRF_REVISION=${GRF_VERSION:-9999}
+BUMP_REVISION=0
+DELETE_NML_FILE=0
+
+_print_help() {
+    cat >&2 << EOF
+    compile.sh - a script to compile xUSSR GRF files
+
+    Usage: $(basename "$0") [options...] <modules to compile>
+      -h, --help             Show this message
+      -b, --bump-revision    Bump revision before compilation
+      -M, --min-revision     Set minimal compatible revision (default: $MIN_COMPATIBLE_REVISION)
+      -B, --basename         Set default GRF basename (default: $NML_BASENAME)
+      -V, --grf-version      Manually set GFR version (when git is not available)
+      -D, --delete-nml       Delete NML-file after the compilation
+
+    Special module names:
+      all - compile all known modules
+      combined - compile combined grf
+EOF
+}
+
+PARSED_ARGS=$(getopt -o "hbM:B:V:D" -l "help,bump-revision,min-revision:,basename:,grf-version:,delete-nml" -- "$@")
+if [[ $? -ne 0 ]]; then
+    _print_help
+    exit 1;
+fi
+
+eval set -- "$PARSED_ARGS"
+
+while [ : ]; do
+    case "$1" in
+        -h | --help)
+            _print_help
+            exit 0
+            ;;
+        -b | --bump-revision)
+            BUMP_REVISION=1
+            shift
+            ;;
+        -M | --min-revision)
+            MIN_COMPATIBLE_REVISION=${2}
+            shift 2
+            ;;
+        -B | --basename)
+            NML_BASENAME=${2}
+            shift 2
+            ;;
+        -V | --grf-version)
+            GRF_VERSION=${2}
+            shift 2
+            ;;
+        -D | --delete-nml)
+            DELETE_NML_FILE=1
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+    esac
+done
+
+echo_err() {
+    echo "$@" >&2
+}
+
+if [ $# -eq 0 ]; then
+    echo_err "No modules to build. Please provide at least one"
+    exit 1
+fi
+
+# generate version string
 if [ -z "${GRF_VERSION}" ]; then
-    GRF_VERSION="$(git describe --tags --dirty="+develop").r${GRF_REVISION}"
+    GRF_VERSION="$(git describe --tags --dirty="+develop")"
     GIT_IS_AVAILABLE=$?
     if [ $GIT_IS_AVAILABLE -ne 0 ]; then
         echo "Git is not installed. Please specify GRF_VERSION as env variable manually" >&2
@@ -18,11 +88,6 @@ if [ -z "${GRF_VERSION}" ]; then
     fi
 fi
 ####
-
-
-echo_err() {
-    echo "$@" >&2
-}
 
 # $1 for subproject (rails, emu, dmu...)
 # $2 for title
@@ -32,10 +97,15 @@ grf_compile() {
     else
         NMLNAME="$NML_BASENAME-$1"
     fi
+    if [ -f "versions/xussr-$1.ver" ]; then
+        GRF_REVISION=$(cat "versions/xussr-$1.ver")
+    else
+        GRF_REVISION=1
+    fi
 
     # create tags
     echo "VERSION: $GRF_REVISION" > "$CUSTOM_TAGS_FILE"
-    echo "TITLE: ${2:-xUSSR Set} $GRF_VERSION" >> "$CUSTOM_TAGS_FILE"
+    echo "TITLE: ${2:-xUSSR Set} ${GRF_VERSION}.r${GRF_REVISION}" >> "$CUSTOM_TAGS_FILE"
     echo "MIN_COMPATIBLE_REVISION: $MIN_COMPATIBLE_REVISION" >> "$CUSTOM_TAGS_FILE"
     echo "FILENAME: $NMLNAME" >> "$CUSTOM_TAGS_FILE"
 
@@ -62,24 +132,36 @@ grf_compile() {
     rm -f "$NMLNAME.nml"
 }
 
-# prepare() {
-    # replacement for clean-lng.pl
-    # replacement for change.pl
-    # gg
-# }
+declare -A XUSSR_MODULE_NAMES=(
+    ["rails"]="xUSSR Rails Set"
+    ["addon"]="xUSSR Railway Set Addon"
+    ["diesel"]="xUSSR Railway Diesels Set"
+    ["dmu"]="xUSSR Railway DMUs Set"
+    ["electric"]="xUSSR Railway Electrics Set"
+    ["emu"]="xUSSR Railway EMUs Set"
+    ["steam"]="xUSSR Railway Steamers Set"
+    ["wagons"]="xUSSR Railway Wagons Set"
+    ["cars"]="xUSSR Railway Cars Set"
+)
 
-monalise() {
-    pushd lang
-    perl scripts/MonaLisa.pl
-    popd
-}
+for grf_module in "$@"; do
+    if [[ "$grf_module" == "combined" ]]; then
+        echo "Compiling $NML_BASENAME..."
+        grf_compile
+        continue
+    fi
 
-grf_compile rails "xUSSR Rails Set"
-# grf_compile addon "xUSSR Railway Set addon"
-grf_compile diesel "xUSSR Railway Diesels Set"
-grf_compile dmu "xUSSR Railway DMUs Set"
-grf_compile electric "xUSSR Railway Electrics Set"
-grf_compile emu "xUSSR Railway EMUs Set"
-grf_compile steam "xUSSR Railway Steamers Set"
-grf_compile wagons "xUSSR Railway Wagons Set"
-grf_compile cars "xUSSR Railway Cars Set"
+    if [[ "$grf_module" == "all" ]]; then
+        echo "Compiling all known modules"
+        for known_module in "${!XUSSR_MODULE_NAMES[@]}"; do
+            echo "Compiling $NML_BASENAME-$known_module..."
+            grf_compile "$known_module" "${XUSSR_MODULE_NAMES["$known_module"]}"
+        done
+        continue
+    fi
+
+    echo "Compiling $NML_BASENAME-$grf_module..."
+    TMP_GRF_NAME=${XUSSR_MODULE_NAMES["$grf_module"]}
+    [ -z "${TMP_GRF_NAME}" ] && TMP_GRF_NAME="$grf_module"
+    grf_compile "$grf_module" "$TMP_GRF_NAME"
+done
